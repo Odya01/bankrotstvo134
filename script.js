28b677276a8967bbb7ae6ad4ae58bac4e03f8ccf
Odya01 <republiquez25@gmail.com>
2024-10-28 00:32:42 +0300

    add discount and adaptiv

--- script.js
+++ script.js
@@ -85,3 +85,83 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// timer
+document.addEventListener("DOMContentLoaded", () => {
+  // 31 декабря 2023 года, 23:59:59
+  const countdownElement = document.getElementById("countdown");
+  const discountBlock = document.querySelector(".welcome__discount");
+  const endTime = new Date("December 31, 2024 23:59:59").getTime();
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const timeLeft = endTime - now;
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    countdownElement.innerHTML = `${days}д ${hours
+      .toString()
+      .padStart(2, "0")}ч ${minutes.toString().padStart(2, "0")}м`;
+    if (timeLeft < 0) {
+      clearInterval(countdownInterval);
+      discountBlock.style.display = "none";
+    }
+  }
+
+  const countdownInterval = setInterval(updateCountdown, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerBtn = document.querySelector(".burger");
+  const burgerMenu = document.querySelector(".nav__burger");
+  const navLink = document.querySelectorAll(".nav__list");
+
+  burgerBtn.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    document.body.classList.toggle("overflow");
+  });
+
+  // close link click
+  navLink.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (burgerMenu.classList.contains("active")) {
+        burgerMenu.classList.remove("active");
+        document.body.classList.remove("overflow");
+        document.getElementById("burger-checkbox").checked = false;
+      }
+    });
+  });
+
+  // close out nav list
+  document.querySelector("main").addEventListener("click", () => {
+    if (burgerMenu.classList.contains("active")) {
+      burgerMenu.classList.remove("active");
+      document.body.classList.remove("overflow");
+      document.getElementById("burger-checkbox").checked = false;
+    }
+  });
+});
+
+// scroll animation
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, options);
+  sections.forEach((section) => {
+    section.classList.add("hiddenSec");
+    observer.observe(section);
+  });
+});
