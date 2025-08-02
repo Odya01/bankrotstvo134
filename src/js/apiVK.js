eef94cac2af8c66cb5aa5a4c9890886327e8cd10
Odya01 <republiquez25@gmail.com>
2025-08-02 21:42:12 +0300

    fix

---
+++ src/js/apiVK.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const vkBlock = document.querySelector(".blog__post_vk");
+  if (!vkBlock) return;
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          loadVkScript();
+          observer.unobserve(vkBlock);
+        }
+      });
+    },
+    {
+      rootMargin: "0px 0px 200px 0px",
+      threshold: 0,
+    }
+  );
+
+  observer.observe(vkBlock);
+
+  function loadVkScript() {
+    const vkScript = document.createElement("script");
+    vkScript.src = "https://vk.com/js/api/openapi.js?169";
+    vkScript.onload = () => {
+      VK.Widgets.Group(
+        "vk_groups",
+        {
+          mode: 4,
+          width: "auto",
+          height: 600,
+          color1: "FFFFFF",
+          color2: "000000",
+          color3: "5181B8",
+        },
+        199689722
+      );
+    };
+    document.body.appendChild(vkScript);
+  }
+});
